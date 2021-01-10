005c29507cca4ef964386911a081ce3a2d9a583b
Artem Moshnin <tap.kap.tap@gmail.com>
2021-01-10 10:23:01 +0100

    safe save

---
+++ server/src/config/alpaca.ts
@@ -0,0 +1,5 @@
+export default {
+  ALPACA_API_KEY_ID: "PKQ4YMKUE9YJVN24TR40",
+  ALPACA_API_SECRET_KEY: "2MjClnWFgRgFcNYMOxvnBgshVUsyJF3W8CPbTkGu",
+  BASE_URL: "https://paper-api.alpaca.markets/",
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("I AM G")
+import alpaca from "@master-chief/alpaca"
+import alpacaConfig from "./config/alpaca"
+
+const client = new alpaca.AlpacaClient({
+  credentials: {
+    key: alpacaConfig.ALPACA_API_KEY_ID,
+    secret: alpacaConfig.ALPACA_API_SECRET_KEY,
+  },
+})
+
+const getData = async () => {
+  const account = await client.getAccount()
+
+  console.log(await client.isAuthenticated())
+
+  console.log(account.buying_power) // number
+  console.log(account.raw().buying_power) // string
+}
+
+getData()
